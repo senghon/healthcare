9aa013e3e553c3fe53537463dce331be9ccbb5b7
senghon <ecovet91@naver.com>
2023-07-05 16:47:24 +0900

    v1

--- src/Routers/Patient.js
+++ src/Routers/Patient.js
@@ -1,28 +1,40 @@
-import React from "react"
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getPatientinfo } from './store';
 
-function Patient() {
-    return (
-      <tbody>
-        {/* 동기처리 */}
-        {Todaypatient.map(function (patient) {
-          return (
-  
-            <tr>
-              <td>{patient.ptname}</td>
-              <td>
-                <p>일자 : {patient.hplrdt}</p>
-                <p>성별 : {patient.ptsxid}</p>
-                <p>품종 : {patient.ptbrid[0]}</p>
-                <p>보호자 : {patient.ptclid}</p>
-                <p>담당 수의사 :{patient.emname}</p>
-              </td>
-            </tr>
-  
-          )
-        }
-        )
-        }
-  
-      </tbody>
-    )
+const Patient = () => {
+  const { ptid } = useParams(); // URL에서 ptid 매개변수를 가져옵니다.
+  const dispatch = useDispatch();
+  const patientInfo = useSelector((state) => state.PatientInfo);
+
+  useEffect(() => {
+    dispatch(getPatientinfo(ptid)); // ptid를 매개변수로 getPatientinfo 액션을 dispatch합니다.
+  }, [dispatch, ptid]);
+
+  if (patientInfo.loading === 'pending') {
+    return <p>Loading...</p>;
   }
+
+  if (patientInfo.error) {
+    return <p>Error: {patientInfo.error}</p>;
+  }
+
+  if (!patientInfo.data) {
+    return null;
+  }
+
+  const { patientName, age, gender, address } = patientInfo.data;
+
+  return (
+    <div>
+      <h1>환자 정보</h1>
+      <p>이름: {patientName}</p>
+      <p>나이: {age}</p>
+      <p>성별: {gender}</p>
+      <p>주소: {address}</p>
+    </div>
+  );
+};
+
+export default Patient;
--- src/store.js
+++ src/store.js
@@ -2,38 +2,41 @@ import { configureStore, createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { useQuery } from "react-query";
 import axios from "axios";
 
-// let todaypatient = useQuery(['result'], () =>
-//     axios.get('http://127.0.0.1:8000/api/today_patients')
-//     .then((a)=>{
-//       return a.data
-//     }))
+//오늘의 환자 목록 로딩
+export const getTodaypatient = createAsyncThunk(
+  "get/todaypatient",
+  async () => {
+    let res = await axios.get("http://localhost:8000/request_today_patient/20211113").then(a => a.data);
+    return res;
+  }
+);
 
-// 비동기적으로 서버에서 오늘 환자 데이터를 get 한다.
-const fetchTodaypatient = createAsyncThunk(
-    "todaypatient가져온다",
-    async()=>{
-        let response = await axios.get('http://127.0.0.1:8000/request_today_patient/20230527').then((a)=>{return a.data})
-        return response
+//각 환자 정보 로딩
+export const getPatientinfo = createAsyncThunk(
+    'get/patientinfo',
+    async (patientid,vsid)=> {
+        let res = await axios.get(`http://localhost:8000/${patientid}/${vsid}`)
     }
 )
 
-// 오늘의 환자 데이터 state를 만든다.
-let Todaypatient = createSlice({
-    name : 'Todaypatient',
-    initialState : [],
-    extraReducers : (builder) =>{
-        builder
-        .addCase(fetchTodaypatient.fulfilled,(state,action) =>{
-            return action.payload;})
-        }
-    }
-)
+const TodaypatientSlice = createSlice({
+  name: "Todaypatient",
+  initialState: {},
+  extraReducers: (builder) => {
+    builder.addCase(getTodaypatient.pending, (state) => {
+      state.loading = "pending";
+    });
+    builder.addCase(getTodaypatient.fulfilled, (state, action) => {
+      state.loading = "suceed";
+      state.user = action.payload;
+    });
+  },
+});
 
+const store = configureStore({
+  reducer: {
+    Todaypatient: TodaypatientSlice.reducer,
+  },
+});
 
-export default configureStore({
-    reducer:{
-        Todaypatient : Todaypatient.reducer,
-    }
-})
-
-export {fetchTodaypatient} 
+export default store;
